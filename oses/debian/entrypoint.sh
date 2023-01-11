dd51667eb19c92e8541255b7367eded551a0052b
Flo <62435410+FRFlo@users.noreply.github.com>
2023-01-11 20:35:06 +0100

    Ajout Debian

---
+++ oses/debian/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Output Current Java Version
+java -version
+
+# Update Current Git Version
+git --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
